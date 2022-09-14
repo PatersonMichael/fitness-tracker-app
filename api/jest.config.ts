9810887d7219d9987967457744c71609032ebb71
craigspaterson <craigspaterson@outlook.com>
2022-09-14 09:06:17 -0700

    update api yaml

---
+++ api/jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from '@jest/types';
+
+// Sync object
+const config: Config.InitialOptions = {
+  verbose: true,
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  // setupFilesAfterEnv: ['./tests/bootstrap.ts']
+};
+
+export default config;
